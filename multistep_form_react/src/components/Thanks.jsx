5a209d2a7e1a90a7233c61960a68d823a7cbd92b
Matheus Antonio <dev.matheus.antonio@outlook.com>
2024-12-13 23:33:48 -0300

    feat
    create styles ReviewForm and Thanks.
    data persistence.

--- multistep_form_react/src/components/Thanks.jsx
+++ multistep_form_react/src/components/Thanks.jsx
@@ -1,9 +1,37 @@
-import React from "react";
+import {
+  BsFillEmojiHeartEyesFill,
+  BsFillEmojiSmileFill,
+  BsFillEmojiNeutralFill,
+  BsFillEmojiFrownFill,
+} from "react-icons/bs";
 
-const Thanks = () => {
+import "./Thanks.css";
+
+const emojiData = {
+  unsatisfied: <BsFillEmojiFrownFill />,
+  neutral: <BsFillEmojiNeutralFill />,
+  satisfied: <BsFillEmojiSmileFill />,
+  verySatisfied: <BsFillEmojiHeartEyesFill />,
+};
+
+const Thanks = ({ data }) => {
   return (
-    <div>
-      <h2>Thanks</h2>
+    <div className="thanksContainer">
+      <h2>Flata pouco...</h2>
+      <p>
+        A sua opinião é muito importante, em breve você receberá um cupom de 10%
+        de desconto para a sua próxima compra.
+      </p>
+      <p>Para concluir sua avaliação clique no botão de Enviar abaixo.</p>
+      <h3>Aqui está o resumo da sua avaliação {data.name}:</h3>
+      <p className="reviewData">
+        <span>Satisfação com o produto:</span>
+        {emojiData[data.review]}
+      </p>
+      <p className="reviewData">
+        <span>Comentário:</span>
+        {data.comment}
+      </p>
     </div>
   );
 };
